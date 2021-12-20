bcb97f52ffb4aeee1c0851b31070a3e88d3df169
oussama-smiai <smiai.oussama@gmail.com>
2021-12-20 16:10:04 +0100

    start instagram clone

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+function Header() {
+    return (
+        <div>
+            <h1> I am a header</h1>
+        {/* left */}
+        <div>
+            <Image 
+                src='https://links.papreact.com/ocw' layout="fill"
+            />
+        </div>
+
+        {/* middle */}
+        {/* right */}
+        </div>
+    
+    )
+}
+export default Header
